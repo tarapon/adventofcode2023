288f3a8eb3f298a5167d7c3610a520060ccd7121
Ivan Tarapon <tarapon@gmail.com>
2023-12-11 23:22:36 +0100

    solve day 11, part 2

--- day11/main.go
+++ day11/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	h "advcode2025/helper"
 	"log"
-	"math"
 	"os"
 	"strings"
 )
@@ -16,20 +15,46 @@ func main() {
 
 	res1, res2 := 0, 0
 	lines := strings.Split(string(content), "\n")
-	m := parseInput(lines)
+	m := tMap(parseInput(lines))
+	expand(m)
 
-	galaxies := findGalaxies(expand(m))
+	galaxies := findGalaxies(m)
 
 	forEachPair(galaxies, func(g1, g2 galaxy) {
-		res1 += g1.distanceTo(g2)
+		res1 += m.distance(g1, g2, 2)
+		res2 += m.distance(g1, g2, 1000000)
 	})
 
 	log.Println("distance (simple):", res1)
 	log.Println("distance (advanced):", res2)
 }
 
+type tMap [][]byte
+
 type galaxy struct {
 	i, j int
+}
+
+func (m tMap) distance(a, b galaxy, w int) int {
+	hDist, vDist := 0, 0
+
+	for i := minInt(a.i, b.i); i < maxInt(a.i, b.i); i++ {
+		if m[i][0] == '+' {
+			vDist += w
+		} else {
+			vDist++
+		}
+	}
+
+	for j := minInt(a.j, b.j); j < maxInt(a.j, b.j); j++ {
+		if m[0][j] == '+' {
+			hDist += w
+		} else {
+			hDist++
+		}
+	}
+
+	return hDist + vDist
 }
 
 func forEachPair(galaxies []galaxy, f func(g1, g2 galaxy)) {
@@ -40,54 +65,48 @@ func forEachPair(galaxies []galaxy, f func(g1, g2 galaxy)) {
 	}
 }
 
-func (g galaxy) distanceTo(o galaxy) int {
-	return int(math.Abs(float64(g.i-o.i)) + math.Abs(float64(g.j-o.j)))
-}
-
 func parseInput(lines []string) [][]byte {
 	return h.Map(lines, func(line string) []byte {
 		return []byte(line)
 	})
 }
 
-func insertAt(a []byte, idx int, v byte) []byte {
-	a = append(a, 0)
-	copy(a[idx+1:], a[idx:])
-	a[idx] = v
-	return a
-}
-
-func expand(s [][]byte) [][]byte {
-	d := make([][]byte, 0, len(s))
-
+func expand(s [][]byte) {
 	// expand vertically
 	for i := 0; i < len(s); i++ {
-		d = append(d, s[i])
-
-		if h.All(s[i], func(b byte) bool { return b == '.' }) {
-			d = append(d, s[i])
-		}
-	}
-
-	// expand horizontally
-	for j := len(d[0]) - 1; j >= 0; j-- {
 		allDots := true
 
-		for i := 0; i < len(d); i++ {
-			if d[i][j] != '.' {
+		for j := 0; j < len(s[i]); j++ {
+			if s[i][j] != '.' {
 				allDots = false
 				break
 			}
 		}
 
 		if allDots {
-			for i := 0; i < len(d); i++ {
-				d[i] = insertAt(d[i], j, '.')
+			for j := 0; j < len(s[i]); j++ {
+				s[i][j] = '+'
 			}
 		}
 	}
 
-	return d
+	// expand horizontally
+	for j := 0; j < len(s[0])-1; j++ {
+		allDots := true
+
+		for i := 0; i < len(s); i++ {
+			if s[i][j] != '.' && s[i][j] != '+' {
+				allDots = false
+				break
+			}
+		}
+
+		if allDots {
+			for i := 0; i < len(s); i++ {
+				s[i][j] = '+'
+			}
+		}
+	}
 }
 
 func findGalaxies(m [][]byte) []galaxy {
@@ -96,10 +115,26 @@ func findGalaxies(m [][]byte) []galaxy {
 	for i := 0; i < len(m); i++ {
 		for j := 0; j < len(m[i]); j++ {
 			if m[i][j] == '#' {
-				galaxies = append(galaxies, galaxy{j, i})
+				galaxies = append(galaxies, galaxy{i, j})
 			}
 		}
 	}
 
 	return galaxies
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
 }
